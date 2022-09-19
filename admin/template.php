78ea8606e28234fee3a1d362325a5c9b1744ffe9
Risad212 <hafez.risad@gmail.com>
2022-09-19 11:08:03 +0600

    done

---
+++ admin/template.php
@@ -0,0 +1,17 @@
+<?php include_once('includes/head.php')?>
+    <body class="sb-nav-fixed">
+       <?php include_once('includes/topnav.php')?>
+        <div id="layoutSidenav">
+            <?php include_once('includes/sidebar.php')?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        
+                    </div>
+                </main>
+                <?php include_once('includes/footer.php')?>
+            </div>
+        </div>
+        <?php include_once('includes/script.php')?>
+    </body>
+</html>
