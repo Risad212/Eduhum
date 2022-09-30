ef93559002ffaf5e33d490cc1280fc0b692d46f8
Mohammad Risad <83496132+Risad212@users.noreply.github.com>
2022-09-30 12:07:20 +0600

    getting error

--- admin/view/add_review_view.php
+++ admin/view/add_review_view.php
@@ -1,30 +1,37 @@
-
+<?php
+  if(isset($_POST['add_review'])){
+      $message = $obj->add_review($_POST);
+ }
+?>
 <h2>Add Review</h2>
 <?php if(isset($message)){echo "<div class='alert alert-success' role='alert'>$message</div>";}?>
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label class="small mb-1" for="blog_title">Name</label>
-        <input name="blog_title" class="form-control py-4" id="blog_title" type="text" />
+        <label class="small mb-1" for="review_name">Name</label>
+        <input name="reivew_name" class="form-control py-4" id="review_name" type="text" />
     </div>
 
     <div class="form-group">
-        <label class="small mb-1" for="blog_content">Review</label>
-        <textarea name="blog_content" id="blog_content" cols="30" rows="10" class="form-control py-4"></textarea>
+        <label class="small mb-1" for="review_content">Review</label>
+        <textarea name="review_content" id="review_content" cols="30" rows="10" class="form-control py-4"></textarea>
     </div>
 
     <div class="form-group">
-        <label class="small" for="blog_img">upload Image</label>
-        <input name="blog_img" class="form-control" id="blog_img" type="file" />
+        <label class="small" for="review_img">upload Image</label>
+        <input name="review_img" class="form-control" id="review_img" type="file" />
     </div>
 
-    <select class="my-4 form-group form-select" aria-label="Default select example">
+    <div class="form-group">
+    <label class="small" for="review_rating">Your rating</label>
+    <select class="form-select" aria-label="Default select example" name="reivew_rating" id="review_rating">
         <option selected>Your Reating</option>
-        <option value="1">One Start</option>
-        <option value="2">Two Start</option>
-        <option value="3">Three Start</option>
-        <option value="4">Four Start</option>
-        <option value="5">Five Start</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
     </select>
+    </div>
     <!-- submit --->
     <input type="submit" name="add_review" value="Add Review" class="form control py-2 btn btn-block btn-primary">
 </form>
